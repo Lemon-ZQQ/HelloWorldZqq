fe286db58b0ccd81fbdde3c217120bb01a9a318a
卓凡 <qingqi.zqq@alibaba-inc.com>
2020-10-29 01:25:09 +0800

    Flink

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ package com.ucas.zqq;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("Flink");
         System.out.println("world");
         System.out.println("finally");
 
