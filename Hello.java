52a15bf025b7a79e1ddd8d55f9504a83e716a55f
卓凡 <qingqi.zqq@alibaba-inc.com>
2020-10-29 01:07:22 +0800

    修改

---
+++ Hello.java
@@ -0,0 +1,13 @@
+package com.ucas.zqq;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("world");
+        System.out.println("finally");
+
+        //-- 新测试分支 new_test_branch1
+        String str = "new_test_branch1"
+    }
+}
+
